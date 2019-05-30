d3b9825d9bff3e4b3b540729bd15e6dd0d0adbb0
MrUncle <50101968+MrTamfanX@users.noreply.github.com>
2019-05-30 21:58:09 +0700

    Add files via upload

---
+++ MrRegisterTelepon.sh
@@ -0,0 +1,204 @@
+#!/usr/bin/bash
+#DaftarNomerTeleponDenganKK&KTP
+#Ini Script Punya MrTamfanX Ya Kaga Usah Record!!!
+#By MrTamfanX 
+#Jangan DiRecord Tod!!!
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo  $blue "<|===================================|>"
+echo  $blue "<|       Daftar Telepon KK && KTP    |>"
+echo  $blue "<|                                   |>"
+echo  $blue "<| Name: MrTamfanX    ||    MrTamfanX|>"
+echo  $blue "<| MyTeam: MrTamfanX Cyber Team      |>"
+echo  $blue "<| Contact: +6285779515200           |>"
+echo  $blue "<| Thanks To: MrTamfanX Cyber Team   |>"
+echo  $blue "<|===================================|>"
+sleep 1
+echo  $green "============== $red MENU $green ================="
+echo  $blue "$green 1.Lihat KK Dan KTP Untuk Register $blue"
+echo  $blue "$green 2.info script ini $blue"
+echo  $blue "$green 0.Selesai Lihat KK Dan KTP $blue"
+echo  $green "======= $red SilahkanPilih $green ======="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@MrTamfanX=•>" MrTamfanX;
+
+clear
+if [ $MrTamfanX = 1 ] || [ $MrTamfanX = 01 ]
+then
+echo $red "[+]Scaning..."
+sleep 12
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "No. KTP : 3326165311980001"
+echo "No. KK  : 3326161201060019"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326161804720041"
+echo "No. KK  : 3326161201060019"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326161107550002"
+echo "No. KK  : 3326161810080003"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3213121008800013"
+echo "No. KK  : 3213120307080144"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326165107080002"
+echo "No. KK  : 3326161205070035"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326164207020001"
+echo "No. KK  : 3326160810050005"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326161607930001"
+echo "No. KK  : 3326162305060005"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326160106450001"
+echo "No. KK  : 3326161408060004"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326162708590001"
+echo "No. KK  : 3326160401060017"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326166206990001"
+echo "No. KK  : 3326160901060011"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3213124107650145"
+echo "No. KK  : 3213122503066368"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 6109062803280000"
+echo "No. KK  : 6109061201070000"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326161607350001"
+echo "No. KK  : 3326160608070285"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326164504960001"
+echo "No. KK  : 3326160712050021"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326165010740004"
+echo "No. KK  : 3326160608070205"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326165312080002"
+echo "No. KK  : 3326160612050042"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326165206830041"
+echo "No. KK  : 3326163003070016"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326163012550021"
+echo "No. KK  : 3326160301060031"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3213121107830001"
+echo "No. KK  : 3213120906080180"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326165007800021"
+echo "No. KK  : 3326161210060002"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326160203930001"
+echo "No. KK  : 3326163011050031"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326164906780001"
+echo "No. KK  : 3326160112050005"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326162012540001"
+echo "No. KK  : 3326162712060005"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326164709590041"
+echo "No. KK  : 3326163010060006"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3304081202700003"
+echo "No. KK  : 3304082107090181"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3213125503840002"
+echo "No. KK  : 3213120906080234"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3213124602810009"
+echo "No. KK  : 3213122503067278"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326164508680001"
+echo "No. KK  : 3326162612050059"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326164107360035"
+echo "No. KK  : 3326160608070361"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3213122206860001"
+echo "No. KK  : 3213121711110003"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326164910770001"
+echo "No. KK  : 3326162811050008"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 0.5
+echo "No. KTP : 3326166712500021"
+echo "No. KK  : 3326161712050002"
+echo $cyan "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+sleep 10
+echo $blue"╭──[Apakah Admin Tamfan]>"
+read -p"╰───────root@MrTamfanX=•>" MrTamfanX;
+sleep 3
+echo $blue"[Terima Kasih Tod Silahkan Ngeue Dulu]"
+sleep 4
+echo $blue"╭──[Pencet Enter/Exit Untuk Keluar]>"
+read -p"╰───────root@MrTamfanX=•>" MrTamfanX;
+sleep 6
+fi
+
+clear
+if [ $MrTamfanX = 2 ] || [ $MrTamfanX = 02 ]
+then
+echo $red "Author  : AdminMrTamfanX"
+echo $blue "Contact : mastergc64@gmail.com"
+echo $cyan "Website : https://tutoriatermuxmrtamfanx.blogspot.com/?m=1"
+echo $blue "Github  : https://github.com/MrTamfanX"
+echo $red "Pesan   : Kau Record Punya Saya MrTamfanX Cyber Team Pecah Pala Kau"
+echo $red "pesan   : Gunakanlah Untuk Daftar Telpon Jangan Yang Lain Anjing...!!!"
+sleep 6
+fi
+
+clear
+if [ $MrTamfanX = 0 ] || [ $MrTamfanX = 00 ]
+then
+echo $red "Author  : AdminMrTamfanX"
+echo $blue "Contact : mastergc64@gmail.com"
+echo $cyan "Website : https://tutoriatermuxmrtamfanx.blogspot.com/?m=1"
+echo $blue "Github  : https://github.com/MrTamfanX"
+echo $red "Pesan   : Kau Record Punya Saya MrTamfanX Cyber Team Pecah Pala Kau"
+echo $blue"╭──[Apakah Admin Tamfan]>"
+read -p"╰───────root@MrTamfanX=•>" MrTamfanX;
+sleep 3
+echo $blue"[Terima Kasih Tod Silahkan Ngeue Dulu]"
+sleep 4
+echo $blue"╭──[Pencet Enter/Exit Untuk Keluar]>"
+read -p"╰───────root@MrTamfanX=•>" MrTamfanX;
+sleep 6
+fi
+exit
+done
+
